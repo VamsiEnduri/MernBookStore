dd0647b1661e140e22cc380eabcf7a2f63ec855d
Venkata Vamsi Enduri <126954986+VamsiEnduri@users.noreply.github.com>
2024-03-26 09:58:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors(
-  {
-    origin: ["https://mern-expense-tracker-lake.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  }
+
 ));
 
 app.get("/", (req, res) => {
